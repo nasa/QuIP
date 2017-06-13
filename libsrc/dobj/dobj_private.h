f9583469010ba5d2d6cca4a2df5b42dcfa005d4d
jbmulligan <jeffrey.b.mulligan@nasa.gov>
2017-06-12 22:13:11 -0700

    new file.

---
+++ libsrc/dobj/dobj_private.h
@@ -0,0 +1,9 @@
+#ifndef _DOBJ_PRIVATE_H_
+#define _DOBJ_PRIVATE_H_
+
+// ascii.c
+extern long next_input_int_with_format(QSP_ARG_DECL   const char *pmpt);
+extern double next_input_flt_with_format(QSP_ARG_DECL  const char *pmpt);
+
+#endif // ! _DOBJ_PRIVATE_H_
+
