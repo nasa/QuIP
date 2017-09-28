25b17ab8cfd22a212017c5aee21d0c13dedc0939
Jeff Mulligan <jeffrey.b.mulligan@nasa.gov>
2017-09-28 10:26:08 -0700

    a little progress, almost ready to try with a real platform.

--- libsrc/vectree/fusion.c
+++ libsrc/vectree/fusion.c
@@ -7,25 +7,154 @@
 #include "platform.h"
 #include "subrt.h"
 
+static int indentation_level=0;	// BUG not thread-safe
+
+static void emit_indentation(QSP_ARG_DECL  String_Buf *sbp)
+{
+	int i=indentation_level;
+
+	while(i--)
+		cat_string(sbp,"\t");
+}
+
+static void emit_symbol_for_func(QSP_ARG_DECL  String_Buf *sbp, Vec_Func_Code code)
+{
+	const char *s;
+	switch(code){
+		case FVADD:
+		case FVSADD:
+			s=" + "; break;
+		case FVMUL:
+		case FVSMUL:
+			s=" * "; break;
+		case FVDIV:
+		case FVSDIV:
+			s=" / "; break;
+		case FVSUB:
+		case FVSSUB:
+			s=" - "; break;
+		default:
+			sprintf(ERROR_STRING,
+	"emit_symbol_for_func:  unhandled function %s!?",
+	VF_NAME(FIND_VEC_FUNC(code)));
+			WARN(ERROR_STRING);
+			return;
+	}
+	cat_string(sbp,s);
+
+}
+
+static void emit_ptr_index(QSP_ARG_DECL  String_Buf *sbp, Vec_Expr_Node *enp)
+{
+	// We need to keep track of which arg is which!
+	cat_string(sbp,"idx");
+}
+
 static void emit_kern_arg_decl(QSP_ARG_DECL  String_Buf *sbp, Vec_Expr_Node *enp)
 {
 	assert(enp!=NULL);
 	if( enp == NULL ) return;
 
-fprintf(stderr,"from emit_kern_arg_decl BEGIN\n");
 	switch( VN_CODE(enp) ){
 		case T_DECL_STAT_LIST:
 			emit_kern_arg_decl(QSP_ARG  sbp, VN_CHILD(enp,0) );
+			cat_string(sbp,", ");
 			emit_kern_arg_decl(QSP_ARG  sbp, VN_CHILD(enp,1) );
 			break;
 		case T_DECL_STAT:
-fprintf(stderr,"emit_kern_arg_decl:  declaration statement:\n");
-dump_tree(QSP_ARG  enp);
+//fprintf(stderr,"emit_kern_arg_decl:  declaration statement:\n");
+//dump_tree(QSP_ARG  enp);
+			cat_string(sbp,PREC_NAME(VN_DECL_PREC(enp)));
+			cat_string(sbp," ");
+			emit_kern_arg_decl(QSP_ARG  sbp, VN_CHILD(enp,0) );
+			break;
+		case T_PTR_DECL:
+			cat_string(sbp,"*");
+			cat_string(sbp,VN_STRING(enp));
+			break;
+		case T_SCAL_DECL:
+			cat_string(sbp,VN_STRING(enp));
 			break;
 		default:
 			MISSING_CASE(enp,"emit_kern_arg_decl");
 			break;
 	}
+}
+
+static void emit_kern_body_node(QSP_ARG_DECL  String_Buf *sbp, Vec_Expr_Node *enp)
+{
+	switch( VN_CODE(enp) ){
+		case T_LIT_INT:
+			sprintf(msg_str,"%ld",VN_INTVAL(enp));
+			cat_string(sbp,msg_str);
+			break;
+		case T_LIT_DBL:
+			sprintf(msg_str," %g",VN_DBLVAL(enp));
+			cat_string(sbp,msg_str);
+			break;
+		case T_DECL_STAT:
+			emit_indentation(QSP_ARG  sbp);
+			cat_string(sbp,PREC_NAME(VN_DECL_PREC(enp)));
+			cat_string(sbp," ");
+			emit_kern_arg_decl(QSP_ARG  sbp, VN_CHILD(enp,0) );
+			cat_string(sbp,";\n");
+			break;
+		case T_DYN_OBJ:
+			// assume this is a var holding a scalar!?
+			cat_string(sbp,VN_STRING(enp));
+			break;
+
+		case T_DEREFERENCE:
+			enp = VN_CHILD(enp,0);
+			assert(enp!=NULL);
+			assert(VN_CODE(enp)==T_POINTER);
+
+			cat_string(sbp,VN_STRING(enp));
+			cat_string(sbp,"[");
+			emit_ptr_index(QSP_ARG  sbp,enp);
+			cat_string(sbp,"]");
+
+			break;
+
+		case T_STAT_LIST:
+			emit_kern_body_node(QSP_ARG  sbp, VN_CHILD(enp,0));
+			emit_kern_body_node(QSP_ARG  sbp, VN_CHILD(enp,1));
+			break;
+		case T_ASSIGN:
+			emit_indentation(QSP_ARG  sbp);
+			emit_kern_body_node(QSP_ARG  sbp, VN_CHILD(enp,0));
+			cat_string(sbp," = ");
+			emit_kern_body_node(QSP_ARG  sbp, VN_CHILD(enp,1));
+			cat_string(sbp," ;\n");
+			break;
+		case T_VV_FUNC:
+			cat_string(sbp,"( ");
+			emit_kern_body_node(QSP_ARG  sbp, VN_CHILD(enp,0));
+			emit_symbol_for_func(QSP_ARG  sbp,VN_VFUNC_CODE(enp));
+			emit_kern_body_node(QSP_ARG  sbp, VN_CHILD(enp,1));
+			cat_string(sbp," )");
+			break;
+
+		case T_VS_FUNC:
+			cat_string(sbp,"( ");
+			// scalar should be emitted first...
+			emit_kern_body_node(QSP_ARG  sbp, VN_CHILD(enp,1));
+			emit_symbol_for_func(QSP_ARG  sbp,VN_VFUNC_CODE(enp));
+			emit_kern_body_node(QSP_ARG  sbp, VN_CHILD(enp,0));
+			cat_string(sbp," )");
+			break;
+		default:
+			MISSING_CASE(enp,"emit_kern_body_node");
+			break;
+	}
+}
+
+static void emit_kern_body(QSP_ARG_DECL  String_Buf *sbp, Subrt *srp)
+{
+	cat_string(sbp,"{\n");
+	indentation_level++;
+	emit_kern_body_node(QSP_ARG  sbp, SR_BODY(srp));
+	cat_string(sbp,"}\n");
 }
 
 static void emit_kern_decl(QSP_ARG_DECL  String_Buf *sbp, Subrt *srp)
@@ -56,6 +185,7 @@ String_Buf *fuse_subrt(QSP_ARG_DECL  Subrt *srp)
 	assert(sbp!=NULL);
 
 	emit_kern_decl(QSP_ARG  sbp, srp );
+	emit_kern_body(QSP_ARG  sbp, srp );
 
 	fprintf(stderr,"Kernel source:\n\n%s\n\n",sb_buffer(sbp));
 	return sbp;
