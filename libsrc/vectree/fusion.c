e7a32933aa059b4effc0042c4615ade5dd13966f
Jeff Mulligan <jeffrey.b.mulligan@nasa.gov>
2017-10-06 09:35:13 -0700

    add space after kernel qualifier.

--- libsrc/vectree/fusion.c
+++ libsrc/vectree/fusion.c
@@ -353,6 +353,8 @@ static void emit_kern_decl(QSP_ARG_DECL  String_Buf *sbp, const char *kname, Sub
 	s = (*(PF_STRING_FN( PFDEV_PLATFORM(curr_pdp) ) ))
 			(QSP_ARG  PKS_KERNEL_QUALIFIER );
 	cat_string(sbp,s);	/*"__kernel " (ocl) or "global" (cuda) */
+	if( strlen(s) > 0 )
+		cat_string(sbp," ");
 	cat_string(sbp,"void ");
 	cat_string(sbp,kname);
 	cat_string(sbp,"(");
