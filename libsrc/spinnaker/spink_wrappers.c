00e9d3d29c3415f75c4c2ae8fa3ac6184b2c1277
Jeff Mulligan <jeffrey.b.mulligan@nasa.gov>
2019-05-30 13:37:06 -0700

    don't need these files, new installation on craik ubuntu docker

--- libsrc/spinnaker/spink_wrappers.c
+++ libsrc/spinnaker/spink_wrappers.c
@@ -82,7 +82,11 @@ SPINK_WRAPPER_ONE_ARG(enable_grey_image_stats,ImageStatisticsEnableGreyOnly,spin
 SPINK_WRAPPER_ONE_ARG(enable_rgb_image_stats,ImageStatisticsEnableRgbOnly,spinImageStatistics,hStatistics)
 SPINK_WRAPPER_ONE_ARG(enable_hsl_image_stats,ImageStatisticsEnableHslOnly,spinImageStatistics,hStatistics)
 //SPINK_WRAPPER_ONE_ARG(get_all_image_stats,ImageStatisticsGetAll(
-SPINK_WRAPPER_ONE_ARG(close_avi_recorder,AVIRecorderClose,spinAVIRecorder,hRecorder)
+
+// spinAVIRecorderClose was deprecated, replaced by spinVideoClose
+// same args???
+SPINK_WRAPPER_ONE_ARG(close_avi_recorder,VideoClose,spinVideo,hSpinVideo)
+
 SPINK_WRAPPER_ONE_ARG(invalidate_spink_node,NodeInvalidateNode,spinNodeHandle,hNode)
 
 
